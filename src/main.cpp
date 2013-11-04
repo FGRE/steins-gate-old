fd06940d8058824b68fb46c5a7a9ab91ea4332f8
Mislav Blažević <krofnica996@gmail.com>
2013-11-04 10:32:38 +0100

    sg00_01.nsb is more exciting than boot script. - steins-gate 0.0.1

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char** argv)
 {
     // Note that scripts call .nss files, NOT nsb. This is a hack
-    Game* pGame = new Game({"cg.npa", "nss.npa", "voice.npa"}, "nss/0_boot.nsb");
+    Game* pGame = new Game({"cg.npa", "nss.npa", "voice.npa"}, "nss/sg00_01.nsb");
     pGame->Run();
     delete pGame;
 }
