7910a52ab5052a7858da22bb6596c072e142d176
Mislav Blažević <krofnica996@gmail.com>
2013-11-22 19:20:27 +0100

    Replace compile.sh with cmake

--- compile.sh
+++
@@ -1,4 +0,0 @@
-g++ -I include -lsfml-graphics -lsfml-window -lnpa -lsfml-system \
--ggdb -Wl,-rpath ../libnpengine -Wl,-rpath ../libnpa -I ../libnpengine/include \
--lnpengine -L ../libnpa/ -L ../libnpengine/ -std=c++11 -o steins-gate \
--Wall -Werror src/* -lsfeMovie
