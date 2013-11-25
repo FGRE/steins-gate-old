39cbefb0a241c52c50046e6bec6fe1a82648b253
Mislav Blažević <krofnica996@gmail.com>
2013-11-25 19:04:53 +0100

    Cosmetic fix in mxe.sh

--- mxe.sh
+++ mxe.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 export PATH=$MXE/usr/bin
-i686-pc-mingw32-gcc src/main.c -I ../libnpengine/include/ \
--L ../libnpengine/ -lnpengine -o steins-gate.exe -mwindows
+i686-pc-mingw32-gcc src/main.c -I ../libnpengine/include/ -L ../libnpengine/ -lnpengine -o steins-gate.exe -mwindows
